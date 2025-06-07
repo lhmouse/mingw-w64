fd48aa2e8d32d0b39e026c2a8101d74356a873a7
Kirill Makurin <maiddaisuki@outlook.com>
2025-06-08 00:25:32 +0900

    crt: btowc: control flags passed to MultiByteToWideChar
    For some code pages, flags must be passed as 0 when calling
    MultiByteToWideChar. Pass 0 for such code pages,
    and MB_ERR_INVALID_CHARS for others.
    
    Signed-off-by: Kirill Makurin <maiddaisuki@outlook.com>
    Signed-off-by: LIU Hao <lh_mouse@126.com>

--- mingw-w64-crt/misc/btowc.c
+++ mingw-w64-crt/misc/btowc.c
@@ -22,10 +22,38 @@ wint_t btowc (int c)
   if (cp == 0)
     return (unsigned char) c;
 
+  /* Flags to pass to MultiByteToWideChar */
+  unsigned flags = 0;
+
+  switch (cp)
+  {
+    case 50220: /* ISO-2022-JP */
+    case 50221: /* ISO-2022-JP */
+    case 50222: /* ISO-2022-JP */
+    case 50225: /* ISO-2022-KR */
+    case 50227: /* ISO-2022-CN */
+    case 50229: /* ISO-2022-CN */
+    case 57002: /* x-iscii-de  */
+    case 57003: /* x-iscii-be  */
+    case 57004: /* x-iscii-ta  */
+    case 57005: /* x-iscii-te  */
+    case 57006: /* x-iscii-as  */
+    case 57007: /* x-iscii-or  */
+    case 57008: /* x-iscii-ka  */
+    case 57009: /* x-iscii-ma  */
+    case 57010: /* x-iscii-gu  */
+    case 57011: /* x-iscii-pa  */
+    case 65000: /* UTF-7       */
+      /* no flags allowed */
+      break;
+    default:
+      flags |= MB_ERR_INVALID_CHARS;
+  }
+
   unsigned char ch = c;
   wchar_t wc = WEOF;
 
-  if (!MultiByteToWideChar (cp, MB_ERR_INVALID_CHARS, (char*)&ch, 1, &wc, 1))
+  if (!MultiByteToWideChar (cp, flags, (char*)&ch, 1, &wc, 1))
     return WEOF;
 
   return wc;
